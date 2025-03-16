997bdac6775b8f77fb00ac5ab894d7b947fc1298
Siael Alves <siaelalves@outlook.com>
2025-03-16 15:23:36 -0300

    FEAT Criado inicializador para submódulo `images`;

---
+++ images.php
@@ -0,0 +1,17 @@
+<?php
+namespace images ;
+
+// Enumerador Flip_Direction
+require dirname(__FILE__) . "/Flip_Direction.php" ;
+
+// Classe metadata
+require dirname(__FILE__) . "/metadata.php" ;
+
+// Classe image
+require dirname(__FILE__) . "/image.php" ;
+
+// Classe dimension
+require dirname(__FILE__) . "/dimension.php" ;
+
+// Classe size
+require dirname(__FILE__) . "/size.php" ;
